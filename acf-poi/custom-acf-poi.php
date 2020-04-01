b210ccbe0fd6625f582bb5ad875dc54911e2b9cc
pedram katanchi <pedramkatanchi@webmapp.it>
2020-04-01 17:13:52 +0200

    Added ACF for orders

--- acf-poi/custom-acf-poi.php
+++ acf-poi/custom-acf-poi.php
@@ -45,3 +45,70 @@ acf_add_local_field_group(array(
 ));
 
 endif;
+
+
+// Added ACF field for orders
+if( function_exists('acf_add_local_field_group') ):
+
+	acf_add_local_field_group(array(
+		'key' => 'group_5caf0f4d42775',
+		'title' => 'Aggiungi data di acquisto',
+		'fields' => array(
+			array(
+				'key' => 'wm_order_parid_date',
+				'label' => 'Data di acquisto',
+				'name' => '_paid_date',
+				'type' => 'date_picker',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'display_format' => 'Y/m/d',
+				'return_format' => 'd/m/Y',
+				'first_day' => 1,
+			),
+			array(
+				'key' => 'wm_order_json',
+				'label' => 'Order Json',
+				'name' => 'order_json',
+				'type' => 'textarea',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+				'maxlength' => '',
+				'rows' => '',
+				'new_lines' => '',
+				'readonly' => 1
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'shop_order',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
+		'active' => true,
+		'description' => '',
+	));
+	
+	endif;
