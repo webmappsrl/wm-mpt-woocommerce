a056fcf7214e64d2c0961f9aa3ab0c0a38f2803d
pedram katanchi <pedramkatanchi@webmapp.it>
2020-06-19 14:53:15 +0200

    Added reset to wm-mpt-set-poi-paid-date

--- wm-cli/wm-mpt-set-poi-paid-date.php
+++ wm-cli/wm-mpt-set-poi-paid-date.php
@@ -6,7 +6,7 @@
 # Success: bar qux
 
 /**
- * Gets first argument as POI ID and second argument as YYYY-MM-DD to update POI's paid-date
+ * Gets first argument as POI ID and second argument as YYYY-MM-DD to update POI's paid-date. if the second argument is "reset" it resets the paid_date
  *
  *
  * @when after_wp_load
@@ -17,8 +17,13 @@ $wm_mpt_set_paid_date = function( $args, $assoc_args )
     $paid_date = $args[1];
     
     if ( $paid_date && is_int($poi_id)) {
-        update_field('paid_date',$paid_date,$poi_id);
-        WP_CLI::success( 'POI with ID #'.$poi_id. ' has a new paid_date: '.$paid_date);
+        if ($paid_date == 'reset') {
+            delete_field('paid_date',$poi_id);
+            WP_CLI::success( 'paid_date value of POI with ID #'.$poi_id. ' has has been deleted');
+        } else {
+            update_field('paid_date',$paid_date,$poi_id);
+            WP_CLI::success( 'POI with ID #'.$poi_id. ' has a new paid_date: '.$paid_date);
+        }
     } else {
         WP_CLI::error( 'Either one argument is missing or the first argument is not numeric' );
     }
